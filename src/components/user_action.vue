907e3e157636c00d5b8a7216988217dea8ab2c7f
EnderCaster <mstext0@hotmail.com>
2019-07-26 17:33:24 +0800

    修改退出按钮颜色

--- src/components/user_action.vue
+++ src/components/user_action.vue
@@ -1,26 +1,30 @@
 <template>
-    <Row class="user-actions">
-        <Button @click="handleLogout">{{$t('action.logout')}}</Button>
-    </Row>
+  <Row class="user-actions">
+    <Button @click="handleLogout">{{$t('action.logout')}}</Button>
+  </Row>
 </template>
 <script>
 export default {
-    name: 'user-action',
-    data:function(){
-        return {};
-    },
-    methods:{
-        handleLogout:function(){
-            axios.defaults.headers.common["Authorization"]=undefined;
-            this.$router.push('/login');
-        }
+  name: "user-action",
+  data: function() {
+    return {};
+  },
+  methods: {
+    handleLogout: function() {
+      axios.defaults.headers.common["Authorization"] = undefined;
+      this.$router.push("/login");
     }
-}
+  }
+};
 </script>
 <style lang="less">
-    .user-actions{
-        position: fixed;
-        height: 100%;
-        float: right;
-    }
+.user-actions {
+  position: fixed;
+  height: 100%;
+  float: right;
+  .ivu-btn {
+    background: transparent;
+    color: white;
+  }
+}
 </style>
