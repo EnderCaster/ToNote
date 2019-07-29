584cc1310bcc47706358a351ead14ed7484f7992
EnderCaster <mstext0@hotmail.com>
2019-07-29 19:26:40 +0800

    提取checkLogin到prototype

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ const i18n=new VueI18n({
     messages
 })
 //AXIOS
-Axios.defaults.baseURL = '//api.tonote.me/';
+Axios.defaults.baseURL = '//api.to-note.endercaster.com/';
 // Axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 window.axios=Axios;
 //富文本编辑器
