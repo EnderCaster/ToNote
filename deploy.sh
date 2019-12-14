fe9efe8a7f349b3abf6f73715ca8773f065a9759
EnderCaster <mstext0@hotmail.com>
2019-12-14 14:31:27 +0800

    update deploy shell

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ then
     mkdir -p /opt/serve_root/2note
 fi
 rm -rf dist
-rm -f index_prod.html
 npm run build
-rm -rf /opt/serve_root/2note/*
-cp -r dist /opt/serve_root/2note/dist
-cp index_prod.html /opt/serve_root/2note/index.html
+rm -rf /opt/serve_root/2note
+cp -r dist /opt/serve_root/2note
